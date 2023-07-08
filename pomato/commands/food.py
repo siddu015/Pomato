2619648faa0780da8d01db1830833f627d06a2ac
siddu015 <116783967+siddu015@users.noreply.github.com>
2023-07-08 21:16:42 +0530

    Update food.py

--- pomato/commands/food.py
+++ pomato/commands/food.py
@@ -6,20 +6,24 @@ food_service = FoodService()
 
 app = typer.Typer()
 
+
 @app.command()
 def add(
     name: str = typer.Option(..., prompt=True, help="Enter the food name"),
     restaurant: str = typer.Option(..., prompt=True, help="Enter the restaurant name"),
     price: int = typer.Option(..., prompt=True, help="Enter the price"),
-    is_veg: bool = typer.Option(True, prompt=True, help="Specify if the food is vegetarian (True/False)")
+    is_veg: bool = typer.Option(True, prompt=True, help="Specify if the food is vegetarian (True/False)"),
+    quantity: int = typer.Option(..., prompt=True, help="Enter the quantity")
 ):
-    food_service.add(name, restaurant, price, is_veg)
+    food_service.add(name, restaurant, price, is_veg, quantity)
+
 
 @app.command()
 def remove(
-     name: Annotated[str, typer.Option(prompt=True)]
+    name: Annotated[str, typer.Option(prompt=True)]
 ):
     food_service.remove(name)
+
 
 @app.command()
 def display(
@@ -27,9 +31,12 @@ def display(
 ):
     food_service.display(restaurant)
 
-@app.command()
-def list():
+
+@app.command(name="list")
+def list_foods():
     food_service.list()
+
 
 if __name__ == "__main__":
     app()
+
