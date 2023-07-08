c0edd3010249a0157f1d988078e56bf40c1c50c1
siddu015 <116783967+siddu015@users.noreply.github.com>
2023-07-08 23:38:06 +0530

    Update food.py

--- pomato/services/food.py
+++ pomato/services/food.py
@@ -8,14 +8,13 @@ console = Console()
 class FoodService:
     def add(self, name: str, restaurant: str, price: int, is_veg: bool = True, quantity: int = 0):
         try:
-            restaurant_exists = Restaurant.select().where(Restaurant.name == restaurant).exists()
-            if not restaurant_exists:
-                print(f"Restaurant '{restaurant}' does not exist.")
-                self.add_prompt(name, price, is_veg, quantity)
-                return
-
             restaurant_obj = Restaurant.get(Restaurant.name == restaurant)
-            food = Food.add_food(name, restaurant_obj, price, is_veg, quantity)
+        except Restaurant.DoesNotExist:
+            print(f"Restaurant '{restaurant}' does not exist.")
+            return
+
+        try:
+            food = Food.create(name=name, restaurant=restaurant_obj, price=price, is_veg=is_veg, quantity=quantity)
             print(f"Food '{food.name}' added successfully.")
         except Exception as e:
             print(f"Error adding food: {str(e)}")
@@ -31,44 +30,18 @@ class FoodService:
 
     def remove(self, name: str):
         try:
-            Food.remove_food(name)
+            food = Food.get(name=name)
+            food.delete_instance()
             print(f"Food '{name}' has been removed.")
         except Food.DoesNotExist:
             print(f"Food '{name}' does not exist.")
 
-    def display(self, restaurant: str):
-        try:
-            restaurant_exists = Restaurant.select().where(Restaurant.name == restaurant).exists()
-            if not restaurant_exists:
-                print(f"Restaurant '{restaurant}' does not exist.")
-                return
 
-            restaurant_obj = Restaurant.get(Restaurant.name == restaurant)
-            foods = Food.select().where(Food.restaurant == restaurant_obj)
-
-            if foods.count() == 0:
-                print(f"No food items found for restaurant '{restaurant}'.")
-                return
-
-            table = Table("sl.No.", "Name", "Price", "Is Veg", "Food-id")
-            for i, food in enumerate(foods):
-                table.add_row(
-                    f"{i + 1}",
-                    food.name,
-                    str(food.price),
-                    "Yes" if food.is_veg else "No",
-                    str(food.id),
-                )
-
-            console.print(f"Food items for restaurant '{restaurant}':")
-            console.print(table)
-        except Restaurant.DoesNotExist:
-            print(f"Restaurant '{restaurant}' does not exist.")
 
     def list(self):
-        foods = Food.list_foods()
+        foods = Food.select()
 
-        if not foods:
+        if foods.count() == 0:
             print("No food items found.")
             return
 
