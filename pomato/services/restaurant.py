cd2f01f31bf99088d1af4c8fab1643bfd435c763
siddu015 <116783967+siddu015@users.noreply.github.com>
2023-07-08 23:38:03 +0530

    Update restaurant.py

--- pomato/services/restaurant.py
+++ pomato/services/restaurant.py
@@ -45,25 +45,28 @@ class RestaurantService:
         console.print(table)
 
     def list_foods(self, restaurant_name: str):
-        try:
-            restaurant = Restaurant.get(name=restaurant_name)
-            foods = Food.select().where(Food.restaurant == restaurant)
+        while True:
+            try:
+                restaurant = Restaurant.get(name=restaurant_name)
+                foods = Food.select().where(Food.restaurant == restaurant)
 
-            if foods.count() == 0:
-                print(f"No food items found for restaurant '{restaurant_name}'.")
-                return
+                if foods.count() == 0:
+                    print(f"No food items found for restaurant '{restaurant_name}'.")
+                    return
 
-            table = Table("sl.No.", "Name", "Price", "Is Veg", "Food-id")
-            for i, food in enumerate(foods):
-                table.add_row(
-                    f"{i + 1}",
-                    food.name,
-                    str(food.price),
-                    "Yes" if food.is_veg else "No",
-                    str(food.id),
-                )
+                table = Table("sl.No.", "Name", "Price", "Is Veg", "Food-id")
+                for i, food in enumerate(foods):
+                    table.add_row(
+                        f"{i + 1}",
+                        food.name,
+                        str(food.price),
+                        "Yes" if food.is_veg else "No",
+                        str(food.id),
+                    )
 
-            console.print(f"Food items for restaurant '{restaurant_name}':")
-            console.print(table)
-        except Restaurant.DoesNotExist:
-            print(f"Restaurant '{restaurant_name}' does not exist.")
+                console.print(f"Food items for restaurant '{restaurant_name}':")
+                console.print(table)
+                break
+            except Restaurant.DoesNotExist:
+                print(f"Restaurant '{restaurant_name}' does not exist.")
+                restaurant_name = input("Enter the restaurant name again: ")
